ac18b95beb1c5986ae10f6f92f32772ea4593e77
Andrej <andrej31.10.92@rambler.ru>
2021-06-22 10:25:44 +0300

    after changes

--- src/ru/geekbrains/java3/Car.java
+++ src/ru/geekbrains/java3/Car.java
@@ -1,6 +1,11 @@
 package ru.geekbrains.java3;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
+    CyclicBarrier c;
+    CountDownLatch cdl;
     private static int CARS_COUNT;
     static {
         CARS_COUNT = 0;
@@ -14,8 +19,10 @@ public class Car implements Runnable {
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, CyclicBarrier c, CountDownLatch end, int speed) {
         this.race = race;
+        this.c = c;
+        this.cdl = end;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
@@ -26,11 +33,18 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            c.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        if (cdl.getCount() == 4) {
+            System.out.println(this.name + " - WIN");
+        }
+        cdl.countDown();
     }
 }
+
+
--- src/ru/geekbrains/java3/Tunnel.java
+++ src/ru/geekbrains/java3/Tunnel.java
@@ -1,6 +1,11 @@
 package ru.geekbrains.java3;
 
+import java.util.concurrent.Semaphore;
+
+import static ru.geekbrains.java3.MainClass.CARS_COUNT;
+
 public class Tunnel extends Stage {
+    Semaphore smp = new Semaphore(CARS_COUNT/2);
     public Tunnel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
@@ -11,12 +16,14 @@ public class Tunnel extends Stage {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                smp.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                smp.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
